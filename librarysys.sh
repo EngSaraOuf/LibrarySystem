b12bf63c4843f09727be6729c69f9081ae4637c0
SaraOuf <sara.oofe@gmail.com>
2025-05-13 05:54:51 +0300

    Nora first commit

--- librarysys.sh
+++ librarysys.sh
@@ -2,7 +2,23 @@
 
 # Function to display all available books  NORA id 12123212
 display_books() {
+    echo "Available Books:"
+    echo "---------------"
+    if [ ! -d "books" ]; then
+        echo "Error: 'books' directory not found!"
+        exit 1
+    fi
     
+    books=("books"/*)
+    if [ ${#books[@]} -eq 1 ] && [ ! -f "${books[0]}" ]; then
+        echo "No books available in the library."
+        exit 1
+    fi
+    
+    for book in "${books[@]}"; do
+        echo "- $(basename "$book")"
+    done
+    echo
 }
 
 # Function to get price from prices.txt MONA id 64736432
