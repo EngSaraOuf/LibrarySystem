a6aec6b444490ac4e3799ecc2f48531c3dc83bb3
EngSaraOuf <sara.mo.ouf@gmail.com>
2025-05-13 03:50:15 +0300

    Leader commited

---
+++ librarysys.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Function to display all available books  NORA id 12123212
+display_books() {
+    
+}
+
+# Function to get price from prices.txt MONA id 64736432
+get_price() {
+
+
+    }
+
+
+total_price=0
+# Function to calculate total price Raghad  id 64736432
+calculate_total() {
+
+}
+
+
+
+# Main program    LANA id 64643
+while true; do
+    clear
+    echo "Welcome to Our Library System"
+    echo "============================"
+    
+done
+
+# Checkout
+clear
+echo "Thank you for your purchase!"
+echo "Your total is: ${total_price}SAR"
+echo
+echo "Have a nice day!"
